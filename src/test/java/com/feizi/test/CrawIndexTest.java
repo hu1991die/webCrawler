7d2cd526ed821e3b7651da6589e2680dc51da348
ruanpeng <ruanpeng@netschina.com>
2015-11-24 09:32:46 +0800

    add log4j.properties file, create jUnitTestCase class

---
+++ src/test/java/com/feizi/test/CrawIndexTest.java
@@ -0,0 +1,44 @@
+/**
+  * @Description:扩展说明
+  * @Copyright: 2015 dreamtech.com.cn Inc. All right reserved
+  * @Version: V6.0
+  */
+package com.feizi.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**  
+ * 索引创建测试用例
+ * @Author: feizi
+ * @Date: 2015年11月24日 上午9:23:53 
+ * @Version:V6.0
+ */
+public class CrawIndexTest {
+
+	/**
+	 * 创建索引
+	  * @Discription:扩展说明
+	  * @return void
+	  * @Author: feizi
+	  * @Date: 2015年11月24日 上午9:29:48
+	 */
+	@Test
+	public void testCrawIndex() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * 新增索引
+	  * @Discription:扩展说明
+	  * @return void
+	  * @Author: feizi
+	  * @Date: 2015年11月24日 上午9:29:56
+	 */
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/feizi/test/CrawSearchTest.java
@@ -0,0 +1,37 @@
+/**
+  * @Description:扩展说明
+  * @Copyright: 2015 dreamtech.com.cn Inc. All right reserved
+  * @Version: V6.0
+  */
+package com.feizi.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**  
+ * 索引检索
+ * @Author: feizi
+ * @Date: 2015年11月24日 上午9:24:34 
+ * @Version:V6.0
+ */
+public class CrawSearchTest {
+
+	@Test
+	public void testCrawSearch() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * 获取类型
+	  * @Discription:扩展说明
+	  * @return void
+	  * @Author: feizi
+	  * @Date: 2015年11月24日 上午9:30:15
+	 */
+	@Test
+	public void testGetType() {
+		fail("Not yet implemented");
+	}
+
+}
